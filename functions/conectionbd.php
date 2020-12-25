3825e94ef032b187b7270fd2b9c5a1b0b137e50e
unknown <bdavid2nd@live.com>
2020-12-25 14:59:19 -0600

    se programo la conexion de base de datos.

---
+++ functions/conectionbd.php
@@ -0,0 +1,8 @@
+<?php
+
+    $conn = mysqli_connect("localhost", "root", "", "prueba");
+    $result = mysqli_query($conn, "SELECT * FROM usuarios");
+    $data = mysqli_fetch_assoc($result);
+    echo $data['usr_usuario'];
+
+?>
