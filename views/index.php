f9be8d97ba3351cbfd4289ff8010f02a55e86416
unknown <bdavid2nd@live.com>
2020-12-26 13:24:06 -0600

    Se creo el formulario de inicio de sesion

--- views/index.php
+++ views/index.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Iniciar Sesion</h1>
+    <form action="../functions/login.php" method="post">
+        <label for="">Usuario</label>
+        <input type="text" name="usuario" id="usuario">
+        <label for="">Contraseña</label>
+        <input type="text" name="pass" id="pass">
+        <input type="submit" value="Iniciar Sesion">
+    </form>
 </body>
 </html>
